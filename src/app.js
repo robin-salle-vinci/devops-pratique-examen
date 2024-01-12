5f40ea80b740a5de585fc86153a4ef94c6de4b7e
Sallé Robin <robin.salle@student.vinci.be>
2024-01-12 15:46:36 +0100

    methode checkames with a name with x

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 console.log('Hello world');
 
 function checkNames(names) {
+    names.forEach(name => {
+        if(name.indexOf('x') != -1) {
+            names.pop(name);
+        }
+    });
     return names;
 }
 module.exports = checkNames;
--- src/app.test.js
+++ src/app.test.js
@@ -7,3 +7,8 @@ test('la méthode checkNames reçoit un tableau vide et renvoie un tableau vide'
 test("la méthode checkNames reçoit un tableau contenant Momo, Hadji et Leo et renvoie un tableau contenant Momo, Hadji, Leo", () => {
     expect(checkNames(['Momo','Hadji','Leo'])).toEqual(['Momo','Hadji','Leo']);
 });
+
+test("la méthode checkNames reçoit un tableau contenant DMX, Xzibit, JeanKevin],  et renvoie un tableau contenant JeanKevin", () => {
+    expect(checkNames(['JC', 'Leo', 'xena'] )).toEqual(['JC','Leo']);
+});
+
